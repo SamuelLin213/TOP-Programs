eeaed1d2e52e653aba456ac2ee1537ebef531487
Samuel <samuelddlin@gmail.com>
2022-02-27 19:55:26 +0000

    finish problem 6

--- Foundations/Javascript Basics/04_removeFromArray/removeFromArray.js
+++ Foundations/Javascript Basics/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  let numArg = arguments.length;
+  let newArr = arr;
+  for(let x = newArr.length -1 ; x >= 0; x--)
+  {
+    for(let y = 0; y < numArg; y++)
+    {
+      if(newArr[x] === arguments[y])
+      {
+        newArr.splice(x, 1);
+      }
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
