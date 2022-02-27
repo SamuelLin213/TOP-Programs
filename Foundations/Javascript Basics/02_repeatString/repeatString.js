0bfe5e2d131e9a4962224fb9422ed4de777b7e80
Samuel <samuelddlin@gmail.com>
2022-02-27 19:32:21 +0000

    finish problem 2

--- Foundations/Javascript Basics/02_repeatString/repeatString.js
+++ Foundations/Javascript Basics/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let strVar = '';
+  if(num < 0)
+    return 'ERROR';
 
+  for(let x = 0; x < num; x++)
+  {
+    strVar += str;
+  }
+  return strVar;
 };
 
 // Do not edit below this line
