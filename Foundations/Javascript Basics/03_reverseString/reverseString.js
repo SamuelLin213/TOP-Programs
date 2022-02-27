85c7b2ee06c44af57fccdfd4add73f827d991037
Samuel <samuelddlin@gmail.com>
2022-02-27 19:36:58 +0000

    finish problem 3

--- Foundations/Javascript Basics/03_reverseString/reverseString.js
+++ Foundations/Javascript Basics/03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let strR = '';
 
+  for(let x = str.length-1; x >= 0; x--)
+  {
+    strR += str.charAt(x);
+  }
+
+  return strR;
 };
 
 // Do not edit below this line
