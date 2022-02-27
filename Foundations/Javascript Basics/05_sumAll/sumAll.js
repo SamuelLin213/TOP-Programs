7aeade16bee0296249085345d26690768ff3d0c8
Samuel <samuelddlin@gmail.com>
2022-02-27 20:20:57 +0000

    finish problem 5

--- Foundations/Javascript Basics/05_sumAll/sumAll.js
+++ Foundations/Javascript Basics/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
 
+  if(num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number")
+  {
+    return "ERROR";
+  }
+
+  let low = Math.min(num1, num2);
+  let high = Math.max(num1, num2);
+
+  for(let x = low; x <= high; x++)
+  {
+    sum += x;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
