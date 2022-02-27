f8d044b4b812cf8ed0bd8e5f06fe4b361022b178
Samuel <samuelddlin@gmail.com>
2022-02-27 20:57:12 +0000

    finish problem 7

--- Foundations/Javascript Basics/07_tempConversion/tempConversion.js
+++ Foundations/Javascript Basics/07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  let celsius = (num - 32.0) * 5.0 / 9.0;
 
+  let rounded = Math.round(celsius * 10) / 10;
+
+  // if(celsius == 0.0)
+  //   return 0;
+
+  return rounded;
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  let fahrenheit = num * 9.0 / 5.0 + 32.0;
 
+  let rounded = Math.round(fahrenheit * 10) / 10;
+
+  // if(rounded == 0.0)
+  //   return 0;
+
+  return rounded;
 };
 
 // Do not edit below this line
