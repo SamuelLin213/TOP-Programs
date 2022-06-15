78adb21d11301e91d1400d6be5bad0496919d8e2
Samuel Lin <samuelddlin@gmail.com>
2022-06-15 12:01:11 -0700

    finish calculator

--- Foundations/Javascript Basics/08_calculator/calculator.js
+++ Foundations/Javascript Basics/08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	// const sum = 0;
+	// for(const x of arr)
+	// {
+	// 	sum += x;
+	// }
+	// return sum;
+	return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.length > 0 ? arr.reduce((sum, num) => sum * num) : 0; //don't want initial value: 0 * n = 0
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	let sum = 1;
+	for(let x = 0; x < expo; x++)
+	{
+		sum *= base;
+	}
+	return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0)
+	{
+		return 1;
+	}
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
