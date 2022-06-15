a5e67c2a353636806fc862a54c53f8769cd959e4
Samuel Lin <samuelddlin@gmail.com>
2022-06-15 13:46:44 -0700

    finish fibonacci

--- Foundations/Javascript Basics/10_fibonacci/fibonacci.js
+++ Foundations/Javascript Basics/10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if(num < 1)
+  {
+    return "OOPS";
+  }
 
+  if(num === 1 || num === 2)
+  {
+    return 1;
+  }
+  else{
+    let first = 1;
+    let second = 1;
+    for(let x = 2; x < num; x++)
+    {
+      let temp = second;
+      second += first;
+      first = temp;
+    }
+    return second;
+  }
 };
 
 // Do not edit below this line
