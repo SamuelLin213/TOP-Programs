20be9b27dfd15922044e926c665a7ec0f1013b32
Samuel Lin <samuelddlin@gmail.com>
2022-06-16 20:50:49 -0700

    findOldest

--- Foundations/Javascript Basics/12_findTheOldest/findTheOldest.js
+++ Foundations/Javascript Basics/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.sort(function(a, b) {
+      let aCurr = (a.hasOwnProperty('yearOfDeath')) ? a.yearOfDeath : ((new Date()).getFullYear());
+      let bCurr = (b.hasOwnProperty('yearOfDeath')) ? b.yearOfDeath : ((new Date()).getFullYear());
+      const lastInventor = aCurr - a.yearOfBirth;
+      const nextInventor = bCurr - b.yearOfBirth;
+      return lastInventor > nextInventor ? -1 : 1;
+    })[0];
 };
 
 // Do not edit below this line
